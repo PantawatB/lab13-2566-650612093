978485af69e12b8ca746d657f14bac2f6650ba24
Pantawat <pantawat_b@cmu.ac.th>
2023-08-09 19:02:28 +0700

    ,

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,42 +1,12 @@
 "use client";
 
-import { MovieRow } from "@/components/MovieRow";
-import { movieDB } from "@/libs/movieDB";
-
-export default function SearchResultPage({ params }) {
-  //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
-  // const processedSearchInput = ...
-  const searchInput = params.searchInput;
-  const processedSearchInput = searchInput.replaceAll("%20", " ");
-
-  // tip2 : Use "includes" string method to check substring
-  // Example : "ABC".includes("AB") -> return true
-
-  // tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
-  // to convert movie title and searchInput to lower case
-  const filteredMovies = movieDB.filter((movie) =>
-    movie.title.toLowerCase().includes(processedSearchInput.toLowerCase())
-  );
-
+export default function HomePage() {
   return (
     <div>
-      <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; {processedSearchInput} &quot;
-      </p>
-      <p className="fw-bold fs-4 text-center">
-        Found {filteredMovies.length} result(s)
-      </p>
-      {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
-      {filteredMovies.map((movie, i) => (
-        <MovieRow
-          key={movie.id}
-          id={movie.id}
-          title={movie.title}
-          detail={movie.detail}
-          rating={movie.rating}
-          number={i + 1}
-        />
-      ))}
+      <div className="text-center">
+        <p className="fw-bold fs-4">Welcome to TOP 10 IMDB</p>
+        <p>We show top 10 movies of all time!</p>
+      </div>
     </div>
   );
 }
