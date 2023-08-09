40f3928e8c8fdc746a6a71b989b170796784fa4b
Pantawat <pantawat_b@cmu.ac.th>
2023-08-09 19:03:25 +0700

    c

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,11 +1,40 @@
 "use client";
 
-export default function HomePage() {
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SearchPage() {
+  const [searchInput, setSearchInput] = useState("");
+
+  //A hook that helps navigating each route programmatically
+  const router = useRouter();
+
+  const searchInputOnChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const searchInputOnKeyUp = (event) => {
+    if (event.key === "Enter") searchBtnOnClick();
+  };
+
+  const searchBtnOnClick = () => {
+    router.push("/search/" + searchInput);
+  };
+
   return (
     <div>
-      <div className="text-center">
-        <p className="fw-bold fs-4">Welcome to TOP 10 IMDB</p>
-        <p>We show top 10 movies of all time!</p>
+      <p className="fw-bold fs-4 text-center">Search Movie</p>
+      <div className="d-flex gap-2">
+        <input
+          className="form-control"
+          placeholder="Search movie..."
+          onChange={searchInputOnChange}
+          onKeyUp={searchInputOnKeyUp}
+          value={searchInput}
+        />
+        <button className="btn btn-primary" onClick={searchBtnOnClick}>
+          Search
+        </button>
       </div>
     </div>
   );
